f21007f50b6f3ff29821899bfc42aa6fadc69311
Mike Garabedian <mike.garabedian@riverbed.com>
2014-10-24 14:52:55 -0400

    Cleanup upload example script

--- examples/shark_upload_pcap.py
+++ examples/shark_upload_pcap.py
@@ -6,22 +6,35 @@
 # accompanying the software ("License").  This software is distributed "AS IS"
 # as set forth in the License.
 
+import os
 
 from steelscript.netshark.core.app import NetSharkApp
 
 
 class UploadPcap(NetSharkApp):
-    def add_option(self, parser):
+    def add_options(self, parser):
         super(UploadPcap, self).add_options(parser)
-        parser.set_usage('%prog SHARK FILE DEST')
+        parser.add_option('--filepath',
+                          help='path to pcap tracefile to upload')
+        parser.add_option('--destname', default=None,
+                          help='location to store on server, defaults to '
+                               '<username>/<basename of filepath>')
 
     def validate_args(self):
         super(UploadPcap, self).validate_args()
-        if len(self.args) != 3:
-            self.parser.error('wrong number of arguments')
+
+        basedir = '/' + self.options.username
+
+        if not self.options.destname:
+            dst = os.path.join(basedir,
+                               os.path.basename(self.options.filepath))
+            self.options.destname = dst
 
     def main(self):
-        self.netshark.upload_trace_file(self.args[2], self.args[1])
+        self.netshark.upload_trace_file(
+            path=self.options.destname,
+            local_file=self.options.filepath
+        )
 
 
 if __name__ == "__main__":
