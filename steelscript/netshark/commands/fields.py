fd52e12ebc36bc415a6e8dda0754a5aeaabe6d7f
Mike Garabedian <mike.garabedian@riverbed.com>
2015-06-25 16:03:28 -0400

    Create steel fields command

--- steelscript/netshark/commands/fields.py
+++ steelscript/netshark/commands/fields.py
@@ -7,10 +7,8 @@
 # as set forth in the License.
 
 
-
 """
-List all the key and column fields that the given netshark appliance supports.
-For full field details, use the -v flag.
+List all the key and column fields that the given NetShark appliance supports.
 """
 
 import optparse
@@ -19,31 +17,26 @@ from steelscript.netshark.core.app import NetSharkApp
 from steelscript.common.datautils import Formatter
 
 
-class FieldsApp(NetSharkApp):
+class Command(NetSharkApp):
+    help = 'List available NetShark extractor fields'
 
     def add_options(self, parser):
-        super(FieldsApp, self).add_options(parser)
+        super(Command, self).add_options(parser)
         group = optparse.OptionGroup(parser, "Column output options")
-        group.add_option('--sort-id', default=False, action='store_true',
-                            help='sort by ID column instead of description')
         group.add_option('--truncate', default=False, action='store_true',
-                            help='truncate description column rather than wrapping')
-        group.add_option('-w', '--table-width', dest='table_width', default=120,
-                            help="max width of table output, defaults to 120 characters")
+                         help="truncate description column, don't wrap")
+        group.add_option('-w', '--table-width', default=120,
+                         help="max char width of table output, default: 120")
         parser.add_option_group(group)
 
     def main(self):
         headers = ['ID', 'Description', 'Type']
-        data = [(f.id, f.description, f.type) for f in self.netshark.get_extractor_fields()]
-        if self.options.sort_id:
-            data.sort()
+        data = [(f.id, f.description, f.type)
+                for f in self.netshark.get_extractor_fields()]
+        data.sort()
         Formatter.print_table(data,
                               headers,
                               padding=2,
                               max_width=int(self.options.table_width),
                               long_column=1,
                               wrap_columns=(not self.options.truncate))
-
-
-if __name__ == '__main__':
-    FieldsApp().run()
