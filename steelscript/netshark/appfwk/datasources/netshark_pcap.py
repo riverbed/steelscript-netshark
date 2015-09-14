efc3bb6ea99007bf5b105d4c7870e74ebe1ba7bd
wenxuan guo <wguo@riverbed.com>
2015-09-14 17:15:23 -0400

    Revamp NetsharkPcapTable to use DownloadTable class

--- steelscript/netshark/appfwk/datasources/netshark_pcap.py
+++ steelscript/netshark/appfwk/datasources/netshark_pcap.py
@@ -7,11 +7,13 @@
 
 from __future__ import division
 
+import os
 import re
 import time
 import math
 import copy
 import pandas
+import shutil
 import logging
 import datetime
 import subprocess
@@ -19,6 +21,8 @@ import multiprocessing
 
 from django import forms
 from functools import wraps
+from django.conf import settings
+
 
 from steelscript.netshark.core.filters import TimeFilter
 from steelscript.netshark.core._class_mapping import path_to_class
@@ -26,7 +30,7 @@ from steelscript.netshark.core._class_mapping import path_to_class
 from steelscript.appfwk.apps.devices.devicemanager import DeviceManager
 from steelscript.appfwk.apps.devices.forms import fields_add_device_selection
 from steelscript.appfwk.apps.datasource.models import \
-    Column, TableField, Table
+    Column, TableField, Table, DatasourceTable, TableQueryBase
 from steelscript.appfwk.apps.datasource.forms import \
     fields_add_time_selection, fields_add_resolution
 from steelscript.appfwk.libs.fields import Function
@@ -43,22 +47,26 @@ from steelscript.netshark.core.filters import BpfFilter
 
 logger = logging.getLogger(__name__)
 
+SPLIT_DIR_PREFIX = '/tmp/splits_'
+PCAP_DIR = 'data/datacache/pcaps'
+
+
+def add_pcap_dir(f):
+    return '/'.join([PCAP_DIR, f])
+
+
 # decorator for bounded methods
 # "http://stackoverflow.com/questions/306130
 # /python-decorator-makes-function-forget-that-it-belongs-to-a-class"
 
-
-SPLIT_DIR_PREFIX = '/tmp/splits_'
-
-
 def logtime(func):
     @wraps(func)
     def inner(*args, **kwargs):
-        logger.debug("NetSharkPcapQuery: %s starting" % func.__name__)
+        logger.debug("%s starting" % func.__name__)
         start_time = time.time()
         ret = func(*args, **kwargs)
         end_time = time.time()
-        logger.debug("NetSharkPcapQuery: %s finished. It took %s."
+        logger.debug("%s finished. It took %s."
                      % (func.__name__,
                         datetime.timedelta(0, end_time - start_time)))
         return ret
@@ -94,14 +102,8 @@ class NetSharkPcapTable(AnalysisTable):
     FIELD_OPTIONS = {'resolution': '1s',
                      'resolutions': ('1s', '1m', '15min', '1h')}
 
-    @classmethod
-    def create(cls, name, wireshark_table, **kwargs):
-        kwargs['tables'] = {}
-        kwargs['related_tables'] = {'basetable': wireshark_table}
-        return super(NetSharkPcapTable, cls).create(name, **kwargs)
-
     def post_process_table(self, field_options):
-        self.copy_columns(self.options['related_tables']['basetable'])
+        self.copy_columns(self.options.related_tables['wireshark'])
 
         fields_add_device_selection(self, keyword='netshark_device',
                                     label='NetShark', module='netshark',
@@ -129,46 +131,26 @@ class NetSharkPcapTable(AnalysisTable):
 class NetSharkPcapQuery(AnalysisQuery):
 
     @logtime
-    def download(self, export):
-        export.download(self.filename, overwrite=True)
-
-    @logtime
     def split_pcap(self):
         cpu_num = multiprocessing.cpu_count()
         per_file = int(math.ceil(self.pkt_num/cpu_num))
 
-        subprocess.Popen('rm -rf %s' % self.output_dir, shell=True).wait()
         subprocess.Popen('mkdir %s' % self.output_dir, shell=True).wait()
         cmd = 'editcap -c %s %s %s/' % (per_file, self.filename,
                                         self.output_dir)
-        logger.debug("NetSharkPcapQuery: %s" % cmd)
+        logger.debug("NetSharkPcapQuery1: %s" % cmd)
         subprocess.Popen(cmd, shell=True).wait()
 
-    def analyze(self, jobs):
+    def analyze(self, jobs=None):
 
-        criteria = self.job.criteria
+        job = jobs.values()[0]
 
-        criteria.entire_pcap = True
+        if job.status == Job.ERROR:
+            raise AnalysisException("%s for downloading pcap file failed: %s"
+                                    % (job, job.message))
 
-        netshark = DeviceManager.get_device(criteria.netshark_device)
-
-        self.export_name = str(path_to_class(netshark,
-                                             criteria.netshark_source_name))
-
-        source = netshark.get_capture_job_by_name(self.export_name)
-
-        timefilter = TimeFilter(criteria.starttime, criteria.endtime)
-
-        self.filename = '%s_export.pcap' % re.sub(r'[() ]', '',
-                                                  self.export_name)
-
-        filters = ([BpfFilter(filt) for filt in self.table.options.filters]
-                   or None)
-
-        with netshark.create_export(source, timefilter, filters=filters,
-                                    wait_for_data=self.table.options.wait_for_data,
-                                    wait_duration=self.table.options.wait_duration) as e:
-            self.download(e)
+        download_job_handle = job.data()['handle'][0]
+        self.filename = add_pcap_dir('%s.pcap' % download_job_handle)
 
         pcap = PcapFile(self.filename)
 
@@ -183,18 +165,16 @@ class NetSharkPcapQuery(AnalysisQuery):
 
         min_pkt_num = self.table.options.split_threshold
 
-        basetable = Table.from_ref(
-            self.table.options.related_tables['basetable']
-        )
+        criteria = self.job.criteria
+        criteria.entire_pcap = True
+
+        wt = Table.from_ref(self.table.options.related_tables['wireshark'])
 
         depjobs = {}
-
-        self.output_dir = SPLIT_DIR_PREFIX + self.filename
-
         if self.pkt_num < min_pkt_num:
             # No need to split the pcap file
             criteria.pcapfilename = self.filename
-            job = Job.create(table=basetable, criteria=criteria,
+            job = Job.create(table=wt, criteria=criteria,
                              update_progress=False, parent=self.job)
 
             depjobs[job.id] = job
@@ -202,6 +182,7 @@ class NetSharkPcapQuery(AnalysisQuery):
             logger.debug("NetSharkPcapQuery starting single job")
             return QueryContinue(self.collect, depjobs)
 
+        self.output_dir = SPLIT_DIR_PREFIX + download_job_handle
         self.split_pcap()
 
         split_files = subprocess.check_output('ls %s' % self.output_dir,
@@ -213,10 +194,13 @@ class NetSharkPcapQuery(AnalysisQuery):
 
         for split in split_files:
             # use wireshark table
-            criteria = copy.copy(self.job.criteria)
-            criteria.pcapfilename = '/'.join([self.output_dir, split])
+            ws_criteria = copy.copy(criteria)
+            ws_criteria.pcapfilename = '/'.join([self.output_dir, split])
 
-            job = Job.create(table=basetable, criteria=criteria,
+            # for ease of removing the split directory in collect func
+            ws_criteria.output_dir = self.output_dir
+
+            job = Job.create(table=wt, criteria=ws_criteria,
                              update_progress=False, parent=self.job)
 
             depjobs[job.id] = job
@@ -227,6 +211,13 @@ class NetSharkPcapQuery(AnalysisQuery):
 
     def collect(self, jobs=None):
         dfs = []
+
+        # Removing the temporary split directory if multiple jobs
+        if len(jobs) > 1:
+            output_dir = jobs.values()[0].criteria.output_dir
+            if os.path.exists(output_dir):
+                shutil.rmtree(output_dir)
+
         for jid, job in jobs.iteritems():
             if job.status == Job.ERROR:
                 raise AnalysisException("%s for pcap file %s failed: %s"
@@ -246,3 +237,102 @@ class NetSharkPcapQuery(AnalysisQuery):
         logger.debug("NetSharkPcapQuery: Query ended.")
 
         return QueryComplete(df)
+
+
+class PcapDownloadTable(DatasourceTable):
+    class Meta:
+        proxy = True
+
+    _query_class = 'PcapDownloadQuery'
+
+    TABLE_OPTIONS = {'aggregated': False,
+                     'include_files': False,
+                     'include_interfaces': False,
+                     'include_persistent': False,
+                     'filters': [],
+                     'wait_for_data': False,
+                     'wait_duration': 10
+                     }
+
+    FIELD_OPTIONS = {'resolution': '1s',
+                     'resolutions': ('1s', '1m', '15min', '1h')}
+
+    def post_process_table(self, field_options):
+        fields_add_device_selection(self, keyword='netshark_device',
+                                    label='NetShark', module='netshark',
+                                    enabled=True)
+        fields_add_time_selection(self, show_start=True,
+                                  initial_start_time='now-1m',
+                                  show_end=True,
+                                  show_duration=False)
+        fields_add_resolution(self,
+                              initial=field_options['resolution'],
+                              resolutions=field_options['resolutions'])
+        func = Function(netshark_source_name_choices, self.options)
+        TableField.create(keyword='netshark_source_name', label='Source',
+                          obj=self,
+                          field_cls=forms.ChoiceField,
+                          parent_keywords=['netshark_device'],
+                          dynamic=True,
+                          pre_process_func=func)
+        fields_add_filterexpr(obj=self)
+
+
+class PcapDownloadQuery(TableQueryBase):
+
+    @logtime
+    def download(self, export):
+        export.download(self.filename, overwrite=True)
+
+    @property
+    def all_pcap_size(self):
+        return sum(os.path.getsize(add_pcap_dir(f))
+                   for f in os.listdir(PCAP_DIR) if f.endswith('.pcap'))
+
+    def delete_pcaps(self):
+        while self.all_pcap_size > settings.PCAP_SIZE_LIMIT:
+            self.delete_oldest_pcap()
+
+    def delete_oldest_pcap(self):
+
+        oldest_pcap = min((f for f in os.listdir(PCAP_DIR)
+                           if f.endswith('.pcap')),
+                          key=lambda f: os.stat(add_pcap_dir(f)).st_mtime)
+        os.unlink(add_pcap_dir(oldest_pcap))
+
+        handle = re.sub('\.pcap$', '', oldest_pcap)
+
+        jobs = Job.objects.filter(handle=handle)
+        if jobs:
+            jobs.delete()
+
+    def run(self):
+        criteria = self.job.criteria
+
+        netshark = DeviceManager.get_device(criteria.netshark_device)
+
+        self.export_name = str(path_to_class(netshark,
+                                             criteria.netshark_source_name))
+
+        source = netshark.get_capture_job_by_name(self.export_name)
+
+        timefilter = TimeFilter(criteria.starttime, criteria.endtime)
+
+        handle = Job._compute_handle(self.table, criteria)
+
+        # check if pcaps directory exists, if not make the directory
+        if not os.path.exists(PCAP_DIR):
+            subprocess.Popen('mkdir %s' % PCAP_DIR, shell=True).wait()
+
+        self.delete_pcaps()
+
+        self.filename = add_pcap_dir('%s.pcap' % handle)
+
+        filters = ([BpfFilter(filt) for filt in self.table.options.filters]
+                   or None)
+        with netshark.create_export(
+                source, timefilter, filters=filters,
+                wait_for_data=self.table.options.wait_for_data,
+                wait_duration=self.table.options.wait_duration) as e:
+            self.download(e)
+        return QueryComplete(pandas.DataFrame([dict(handle=handle)]))
